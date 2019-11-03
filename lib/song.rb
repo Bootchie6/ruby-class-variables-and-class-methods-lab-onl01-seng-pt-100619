153dc583a9a87db4a43c69b70936ee8a187efe0a
Jonathan Jopio <jonathanjopio@gmail.com>
2019-11-03 23:15:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,15 @@ class Song
   end
   
   def self.artist.count
-    
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
 
